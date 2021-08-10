26e38bd5ef8a072436925ee8fe4972a73f320369
Anoop Jose <anoop.jose@sobharealty.com>
2021-08-10 12:43:20 +0400

    migration-created

---
+++ database/migrations/2021_08_10_082403_create_route_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRouteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('route', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id');
+            $table->string('route_no');
+            $table->string('route_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('route');
+    }
+}
